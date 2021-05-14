97d39ed83e7da21b1fd1630874b95d75a5549f70
gsej <gsej@erskinejones.com>
2021-05-14 07:33:01 +0100

    factor out some js code;

--- webapp/static/temperature_data.js
+++ webapp/static/temperature_data.js
@@ -1,22 +1,94 @@
 
 class ChartManager {
 
-
-
     constructor(element) {
+        Chart.defaults.elements.point.radius = 0;
 
+        this.data = {
+            datasets: [{
+                label: 'Inside Temperatures',
+                data: [],
+                fill: false,
+                borderColor: 'rgb(255, 0, 0)',
+                borderWidth: 2,
+                tension: 0,
+                spanGaps: 1000 * 60 * 35, // I don't know what this number represents.
+                parsing: {
+                    xAxisKey: 'timestamp',
+                    yAxisKey: 'insideTemperature'
+                }
+            },
+            {
+                label: 'Outside Temperatures',
+                data: [],
+                fill: false,
+                borderColor: 'rgb(0, 255, 0)',
+                borderWidth: 2,
+                tension: 0,
+                spanGaps: 1000 * 60 * 35,
+                parsing: {
+                    xAxisKey: 'timestamp',
+                    yAxisKey: 'outsideTemperature'
+                }
+            }],
+        }
+
+        this.config = {
+            type: 'line',
+
+            data: this.data,
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                plugins: {
+                    legend: {
+                        position: "bottom"
+                    }
+                },
+                scales: {
+                    x: {
+                        weight: 0,
+                        type: 'time',
+                        position: 'bottom',
+                        time: {
+                            displayFormats: {
+                                hour: 'dd MMM HH mm',
+                            }
+                        },
+                        ticks: {
+                            minRotation: 90,
+                            source: 'auto'
+                        }
+                    },
+                }
+            }
+        };
+
+        this.chart = new Chart(element, this.config);
     }
+
+    setDisplayFormat(displayFormat) {
+        this.config.options.scales.x.time.displayFormats.hour = displayFormat;
+    }
+
+    setData(temperaturesToShow) {
+        this.data.datasets[0].data = this.data.datasets[1].data = temperaturesToShow;
+        this.chart.update();
+    }
+}
+
+function setHeading(title) {
+    const heading = document.getElementsByTagName("h1")[0];
+    heading.textContent = title;
+
 }
 
 document.addEventListener('DOMContentLoaded', function () {
 
-    Chart.defaults.elements.point.radius = 0;
-
-    const heading = document.getElementsByTagName("h1")[0];
-
     let allTemperatures;
 
     let defaultSelectedDateRange = "all";
+
 
     getSelectedDateRange = () => {
         let selectedDateRange = window.localStorage.getItem("selectedDateRange");
@@ -33,70 +105,7 @@ document.addEventListener('DOMContentLoaded', function () {
         window.localStorage.setItem("selectedDateRange", value);
     }
 
-    const data = {
-        datasets: [{
-            label: 'Inside Temperatures',
-            data: [],
-            fill: false,
-            borderColor: 'rgb(255, 0, 0)',
-            borderWidth: 2,
-            tension: 0,
-            spanGaps: 1000 * 60 * 35, // I don't know what this number represents.
-            parsing: {
-                xAxisKey: 'timestamp',
-                yAxisKey: 'insideTemperature'
-            }
-        },
-        {
-            label: 'Outside Temperatures',
-            data: [],
-            fill: false,
-            borderColor: 'rgb(0, 255, 0)',
-            borderWidth: 2,
-            tension: 0,
-            spanGaps: 1000 * 60 * 35,
-            parsing: {
-                xAxisKey: 'timestamp',
-                yAxisKey: 'outsideTemperature'
-            }
-        }],
-    }
-
-    const config = {
-        type: 'line',
-
-        data: data,
-        options: {
-            responsive: true,
-            maintainAspectRatio: false,
-            plugins: {
-                legend: {
-                    position: "bottom"
-                }
-            },
-            scales: {
-                x: {
-                    weight: 0,
-                    type: 'time',
-                    position: 'bottom',
-                    time: {
-                        displayFormats: {
-                            hour: 'dd MMM HH mm',
-                        }
-                    },
-                    ticks: {
-                        minRotation: 90,
-                        source: 'auto'
-                    }
-                },
-            }
-        }
-    };
-
-    const chart = new Chart(
-        document.getElementById('chart'),
-        config
-    );
+    const chartManager = new ChartManager(document.getElementById('chart'));
 
     const dateRanges = [
         { key: "last24", label: "Last 24 hours" },
@@ -118,7 +127,7 @@ document.addEventListener('DOMContentLoaded', function () {
             radioButton.setAttribute("autofocus", true);
         }
 
-         radioButton.addEventListener("change", (e) => {
+        radioButton.addEventListener("change", (e) => {
             setSelectedDateRange(e.target.value);
             setTimeout(() => filterTemperatures(), 0);
         });
@@ -136,44 +145,36 @@ document.addEventListener('DOMContentLoaded', function () {
         let temperaturesToShow;
 
         dateRange = dateRanges.find(r => r.key === getSelectedDateRange());
-        heading.innerText = dateRange.label;
+        setHeading(dateRange.label);
 
         if (dateRange.key === "all") {
             temperaturesToShow = allTemperatures;
-            config.options.scales.x.time.displayFormats.hour = 'dd MMM HH mm';
+            chartManager.setDisplayFormat("dd MMM HH mm");
         }
         else if (dateRange.key === "last24") {
             const now = new Date();
             const dayAgo = new Date();
             dayAgo.setHours(dayAgo.getHours() - 24);
-
             temperaturesToShow = allTemperatures.filter(t => t.timestamp >= dayAgo && t.timestamp <= now);
-
-            config.options.scales.x.time.displayFormats.hour = "HH mm"
+            chartManager.setDisplayFormat("HH mm");
         }
         else if (dateRange.key === "today") {
             const today = new Date();
-
             const startOfDay = new Date(today.getFullYear(), today.getMonth(), today.getDate());
             const endOfDay = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 23, 59, 59);
-
             temperaturesToShow = allTemperatures.filter(t => t.timestamp >= startOfDay && t.timestamp <= endOfDay);
-
-            config.options.scales.x.time.displayFormats.hour = "HH mm"
+            chartManager.setDisplayFormat("HH mm");
         }
         else if (dateRange.key === "lastweek") {
             const now = new Date();
             const weekAgo = new Date();
             weekAgo.setDate(weekAgo.getDate() - 7);
-
             temperaturesToShow = allTemperatures.filter(t => t.timestamp >= weekAgo && t.timestamp <= now);
-
-            config.options.scales.x.time.displayFormats.hour = 'dd MMM HH mm';
+            chartManager.setDisplayFormat("dd MMM HH mm");
         }
 
-        data.datasets[0].data = temperaturesToShow;
-        data.datasets[1].data = temperaturesToShow;
-        chart.update();
+        chartManager.setData(temperaturesToShow);
+
     }
 
     fetch('api/temperatures')
