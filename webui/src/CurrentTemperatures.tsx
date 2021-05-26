a9554ca13045e469f37844b339207c3c26b3f38e
gsej <gsej@erskinejones.com>
2021-05-26 21:53:47 +0100

    WIP

--- webui/src/CurrentTemperatures.tsx
+++ webui/src/CurrentTemperatures.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './CurrentTemperatures.css';
+import styles from './CurrentTemperatures.module.css';
 
 interface Props {
   insideTemperature: number | null,
@@ -11,7 +11,7 @@ export class CurrentTemperatures extends React.Component<Props> {
   render() {
 
     return (
-      <div className="temperatures">
+      <div className={styles.temperatures}>
         <div>Current Temperature Inside: <span>{this.props.insideTemperature !== null ? this.props.insideTemperature : 'Unavailable'}</span>&#176;C</div>
         <div>Current Temperature Outside: <span>{this.props.outsideTemperature !== null ? this.props.outsideTemperature : 'Unavailable'}</span>&#176;C</div>
       </div>
