ab4fe259d039cdbf13794f83490262841a50083e
gsej <gsej@erskinejones.com>
2021-11-03 15:40:55 +0000

    create fresh react app and add nginx configuration

--- nginx/install.sh
+++ nginx/install.sh
@@ -5,5 +5,6 @@ cp sites/* /etc/nginx/sites-available
 
 # install the original all python site - python api and python templated site.
 ln -s  /etc/nginx/sites-available/polytunnel-all-python /etc/nginx/sites-enabled
+ln -s  /etc/nginx/sites-available/polytunnel-react /etc/nginx/sites-enabled
 
 systemctl restart nginx
