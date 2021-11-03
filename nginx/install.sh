43ad689268d1de48c188f709b7cd82f68b635f2e
gsej <gsej@erskinejones.com>
2021-11-03 14:56:04 +0000

    clarify nginx site files

---
+++ nginx/install.sh
@@ -0,0 +1,9 @@
+rm /etc/nginx/sites-available/*
+rm /etc/nginx/sites-enabled/*
+
+cp sites/* /etc/nginx/sites-available
+
+# install the original all python site - python api and python templated site.
+ln -s  /etc/nginx/sites-available/polytunnel-all-python /etc/nginx/sites-enabled
+
+systemctl restart nginx
