174e430bff43e0f3e10784367dc319d6b193c32d
gsej <gsej@erskinejones.com>
2022-01-03 08:47:12 +0000

    fix link to github icon

--- webui/src/Navigation.tsx
+++ webui/src/Navigation.tsx
@@ -9,7 +9,7 @@ export const Navigation  = () => {
         <a href="/pi">pi</a>
         <a href="/nginx_status">nginx</a>
         <div className={styles.github}>
-            <a href="https://github.com/gsej/polytunnel-monitor"><img alt="github icon" src="%PUBLIC_URL%/GitHub-Mark-32px.png" /></a>
+            <a href="https://github.com/gsej/polytunnel-monitor"><img alt="github icon" src="GitHub-Mark-32px.png" /></a>
         </div>
     </nav>
   );
