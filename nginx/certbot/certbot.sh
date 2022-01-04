06de27425487a2ccbf36a9313829098039806550
gsej <gsej@erskinejones.com>
2022-01-04 12:07:58 +0000

    certbot script update
    temperature file

--- nginx/certbot/certbot.sh
+++ nginx/certbot/certbot.sh
@@ -1,7 +1,7 @@
 
 # install the dnsmadeeasy plugin as a snap then run:
 
-sudo /snap/bin/certbot certonly --dns-dnsmadeeasy  -d polytunnel.gsej.co.uk -d *.polytunnel.gsej.co.uk
+sudo /snap/bin/certbot certonly --dns-dnsmadeeasy  -d polytunnel.gsej.co.uk -d polytunnel2.gsej.co.uk -d *.polytunnel.gsej.co.uk
 
 
 
