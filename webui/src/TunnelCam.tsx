732fb3530b15a4b74d15239a81ffd32cd179890f
gsej <gsej@erskinejones.com>
2022-01-04 20:39:43 +0000

    wire up tunnelcam

--- webui/src/TunnelCam.tsx
+++ webui/src/TunnelCam.tsx
@@ -1,12 +1,16 @@
+import { FC } from "react";
+import { useTunnelCam } from "./state/tunnelcam/TunnelCamContext";
 import styles from "./TunnelCam.module.css";
 
-export const TunnelCam = () => {
+export const TunnelCam: FC = () => {
+
+  const { latestImageUrl } = useTunnelCam();
   return (
       <div className={styles["tunnelcam-container"]}>
       <section>
         <h1>Tunnelcam - Latest Hourly Image</h1>
         <div className="camera">
-          <img alt="latest view of polytunnel" src="{{ latestImageUrl }}" title="{{ latestImageUrl }}" />
+          <img alt="latest view of polytunnel" src={ latestImageUrl } title={ latestImageUrl } />
         </div>
       </section>
     </div>
