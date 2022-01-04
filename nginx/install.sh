4b81a3dbff3e9b76c02c9bb8a4e8a26f722a4d7e
gsej <gsej@erskinejones.com>
2022-01-04 19:57:30 +0000

    make the react app the primary one in nginx confi

--- nginx/install.sh
+++ nginx/install.sh
@@ -4,7 +4,7 @@ rm /etc/nginx/sites-enabled/*
 cp sites/* /etc/nginx/sites-available
 
 # install the original all python site - python api and python templated site.
-ln -s  /etc/nginx/sites-available/polytunnel-all-python /etc/nginx/sites-enabled
-ln -s  /etc/nginx/sites-available/polytunnel-react /etc/nginx/sites-enabled
+ln -s  /etc/nginx/sites-available/polytunnel-api /etc/nginx/sites-enabled
+ln -s  /etc/nginx/sites-available/polytunnel-webui /etc/nginx/sites-enabled
 
 systemctl restart nginx
