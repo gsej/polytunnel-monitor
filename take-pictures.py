52b5b1c9de8bd288d8b81cca9d646789ecc79b2d
gsej <gsej@erskinejones.com>
2022-02-26 14:03:30 +0000

    flip camera image because camera has been remounted

--- take-pictures.py
+++ take-pictures.py
@@ -6,6 +6,6 @@ currentUtcTime = datetime.datetime.utcnow().isoformat()
 
 jpgFilename = datetime.datetime.utcnow().isoformat() + ".jpg"
 print("taking picture " + jpgFilename)
-os.system("raspistill -o ./photos/" + jpgFilename)
+os.system("raspistill --hflip --vflip -o ./photos/" + jpgFilename)
 
 print("completed")
