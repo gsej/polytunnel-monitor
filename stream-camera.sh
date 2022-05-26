a534ce0097552b7c2a3418f83c47b982725cb0fd
gsej <gsej@erskinejones.com>
2022-05-26 10:47:22 +0100

    temperature files mostly

--- stream-camera.sh
+++ stream-camera.sh
@@ -1,1 +1,1 @@
-raspivid -o - -t 9999999 -w 800 -h 600  --vflip --hflip | cvlc -vvv stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:8080}' :demux=h264
+libcamera-video -o - -t 9999999 -w 800 -h 600  --vflip --hflip | cvlc -vvv stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:8080}' :demux=h264
