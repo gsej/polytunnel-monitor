7f5614c8f2f597540757671b7079b87944c081fe
gsej <gsej@erskinejones.com>
2022-06-08 11:24:06 +0100

    some styling for swagger
    added endpoint

--- webui/src/swagger.tsx
+++ webui/src/swagger.tsx
@@ -1,13 +1,37 @@
 import SwaggerUI from "swagger-ui-react"
 import "swagger-ui-react/swagger-ui.css"
+import style from "styled-components"
+
+const StyledDiv = style.div`
+
+    .scheme-container {
+        background-color: var(--light-color );
+        box-shadow: none;
+    }
+
+
+    .servers {
+        display: inline-block;
+    }
+
+    .servers-title {
+        font-size: 24px;
+    }
+
+    .opblock.opblock-get {
+        background-color: var(--light-color );
+    }
+
+
+
+
+`
 
 export const Swagger = () => {
-
-
     return (
-
-        <SwaggerUI url="swagger.json" />
-
+        <StyledDiv>
+            <SwaggerUI url="swagger.json" />
+        </StyledDiv>
     )
 };
 
