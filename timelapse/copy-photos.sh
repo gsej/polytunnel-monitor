fa7edb1671ebe05cd6ac5377d031090dbf73472a
gsej <gsej@erskinejones.com>
2022-09-11 12:31:48 +0100

    update timelapse scripts

--- timelapse/copy-photos.sh
+++ timelapse/copy-photos.sh
@@ -1,1 +1,1 @@
-scp pi@192.168.1.6:/home/pi/repos/polytunnel-monitor/photos/* /home/gsej/repos/polytunnel-monitor/photos/
+rsync --progress --append --partial -vz 192.168.1.21:/home/pi/repos/polytunnel-monitor/photos/* ../photos
