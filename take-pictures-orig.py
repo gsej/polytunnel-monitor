36cb4da6074b5f95601b4222ff19c9ddd79e10d8
gsej <gsej@erskinejones.com>
2024-03-22 21:52:33 +0000

    temperature files

--- take-pictures-orig.py
+++ take-pictures-orig.py
@@ -7,7 +7,7 @@ currentUtcTime = datetime.datetime.utcnow().isoformat()
 
 jpgFilename = datetime.datetime.utcnow().isoformat() + "Z.jpg"
 print("taking picture " + jpgFilename)
-os.system("libcamera-jpeg --tuning-file /usr/share/libcamera/ipa/rpi/vc4/imx477_af.json --autofocus-mode manual --lens-position 500.0 --o ./photos/" + jpgFilename)
+os.system("libcamera-jpeg --tuning-file /usr/share/libcamera/ipa/rpi/vc4/imx477_af.json --autofocus-mode manual --lens-position 0.0 --o ./photos/" + jpgFilename)
 #os.system("libcamera-jpeg --tuning-file /usr/share/libcamera/ipa/rpi/vc4/imx477_af.json --autofocus-mode manual --lens-position 0.0 --hflip --vflip -o ./photos/" + jpgFilename)
 
 print("completed")
